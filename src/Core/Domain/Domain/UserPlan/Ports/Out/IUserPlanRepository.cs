1445148a838c08f58a54b22cb574be0685ba2292
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2026-03-08 15:25:49 -0300

    feat: adicionando interface que estava fora do tracking

---
+++ src/Core/Domain/Domain/UserPlan/Ports/Out/IUserPlanRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.UserPlan.Ports.Out;
+
+public interface IUserPlanRepository
+{
+    Task CreateAsync(Entities.UserPlan userPlan);
+    Task UpdateAsync(Entities.UserPlan userPlan);
+    void Delete(Entities.UserPlan userPlan);
+    Task<Entities.UserPlan> GetByIdAsync(int id);
+    Task<List<Entities.UserPlan>> GetAllAsync();
+    Task<Entities.UserPlan> GetByUserId(string userId);
+}
